32d0e6c60e0abdd75db2f5c8261501454acf3413
NTomasetti <nathaniel.tomasetti@monash.edu>
2018-02-22 11:09:32 +1100

    plotoz documentation

--- plotOz.R
+++ plotOz.R
@@ -1,14 +1,36 @@
 library(tidyverse)
 library(rlang)
-# Inputs:
-#data: Either Null or a Data frame of polygons and group as a result from ggplot2::fortify
-#state: Either Null or an Australian state code
-#interactive: FALSE: Return ggplot2 object, TRUE: Return ggplotly object
-# fill = User supplied column to fill by
-# label = User supplied column to add to plotly tooltips
-
-# Output:
-# alternatively a ggplot2 or ggplotly object of the mapped shapefile, possibly filtered by state
+#' Plot Australian Map Dataframes
+#'
+#' Wrapper to plot Australia, with functionality to subset by state, fill by a column and add interactivity through plotly
+#'
+#' @param data a dataframe containing long, lat and group columns provided from ggplot2::fortify.
+#'     Additional columns can be added for filtering and plot aesthetics.
+#'     If NULL, a dataframe containing Australia split by States is provided
+#' @param state Either NULL or a character vector containing only elements from
+#'     "ACT", "NSW", "NT", "QLD", "SA", "TAS", "VIC" or "WA". Only the selected states
+#'     or terrictories will be plotted. If NULL all of Australia is plotted.
+#' @param fill Either NULL or a column of the dataframe to fill the plot by.
+#'     If NULL then fill defaults to 'grey90'
+#' @param interactive logical - if FALSE (default) a ggplot2 object is returned. If TRUE a plotly::ggplotly object is returned
+#' @param label Either NULL or a column in the data to include in the plotly tooltips
+#'
+#' @return Either a ggplot2 or ggplotly object
+#' @export
+#'
+#' @examples
+#'
+#' plotOz()
+#' # A plot of Australia in the default colours
+#'
+#' plotOz(state = c('ACT', 'NSW', 'VIC', 'TAS', 'QLD'), fill = pop)
+#' # A plot of the eastern states filled according to population
+#'
+#' plotOz(state = 'VIC', interactive = TRUE, label = pop)
+#' An interactive plot of Victoria with population as a tooltip
+#'
+#' plotOz(fill = pop, interactive = TRUE, label = State)
+#' An interactive plot of Australia with colour filled by population and the State names added to the tooltip
 
 plotOz <- function(data = NULL, state = NULL, fill = NULL, interactive = FALSE, label = NULL){
   #If no shapefile is supplied in data, default to the basic map of Australia with state lines
@@ -41,10 +63,6 @@ plotOz <- function(data = NULL, state = NULL, fill = NULL, interactive = FALSE, 
   }
   return(p)
 }
-plotOz(state = c('NSW', 'VIC', 'TAS', 'QLD'), fill = pop)
-plotOz(fill = pop, interactive = TRUE, label = State)
-plotOz()
-plotOz(state = 'VIC', interactive = TRUE, label = paste(pop, State))
 
 
 
